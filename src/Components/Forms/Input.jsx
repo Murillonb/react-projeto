d6fdd980cc971871c5d49e999f58f390b06cb8c8
murillonb <murillo_099@hotmail.com>
2026-05-04 20:38:01 +0200

    API Fetch

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({
-  label,
-  type,
-  name,
-  value,
-  onChange,
-  validate,
-  onBlur,
-  error,
-}) => {
+const Input = ({ label, type, name, value, onChange, onBlur, error }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
