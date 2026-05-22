749ada714de97b44913f9d131c9917888dd8aa41
murillonb <murillo_099@hotmail.com>
2026-05-22 16:37:24 +0200

    skeleton effect was added to the images

---
+++ src/Components/Photo/PhotoDelete.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './PhotoDelete.module.css';
+import { PHOTO_DELETE } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+
+const PhotoDelete = ({ id }) => {
+  const token = window.localStorage.getItem('token');
+  const { loading, request } = useFetch();
+
+  async function handleClick() {
+    const confirm = window.confirm(
+      'Are you sure you want to delete this photo?',
+    );
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id, token);
+      const { response } = await request(url, options);
+      if (response.ok) window.location.reload();
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Delete
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Delete
+        </button>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;
